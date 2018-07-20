0f66cc32738cf10050c1f9d1a2f540b308399061
Nate Jensen <najens@gmail.com>
2018-07-20 15:02:24 -0600

    docs: Create Article component

---
+++ src/components/Article.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { createUrlTitle } from '../utils/helpers'
+
+const Article = (props) => {
+	const { article } = props
+	const url_title = createUrlTitle(article.title)
+	const path = `article/${article.id}/${url_title}`
+	return (
+		<Link className='article-container' to={path} id={article.id}>
+			<img className='article-img' src={article.images[0].url}/>
+			<header className='article-header'>
+				{article.title}
+			</header>
+		</Link>
+	)
+}
+
+function mapStateToProps({ articles }, { id }) {
+	const article = articles[id]
+	return {
+		article,
+	}
+}
+
+export default connect(mapStateToProps)(Article)
