0b7ac334ab1457320fca974829207c511d2b24c6
Nate Jensen <najens@gmail.com>
2018-07-20 14:59:05 -0600

    docs: Add routes to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navigation from './Navigation'
+import Login from './Login'
+import Dashboard from './Dashboard'
+import ArticlePage from './ArticlePage'
+import ArticleForm from './ArticleForm'
+import TilePage from './TilePage'
+import TileForm from './TileForm'
+import NotFound from './NotFound'
 
-class App extends Component {
+export class App extends Component {
+  componentDidMount() {
+    const { handleInitialData } = this.props
+    handleInitialData()
+  }
   render() {
     return (
-      <div className="App">
-        Hello World
-      </div>
+      <Router>
+        <Fragment>
+          <Navigation />
+            {this.props.loading === true
+              ? null
+              : <Switch>
+                  <Route path='/' exact component={Dashboard} />
+                  <Route path='/login' component={Login} />
+                  <Route path='/article/:id/:title' component={ArticlePage} />
+                  <Route path='/article/add' component={ArticleForm} />
+                  <Route path='/article/:id/edit' component={ArticleForm} />
+                  <Route path='/tile/:id/:title' component={TilePage} />
+                  <Route path='/tile/add' component={TileForm} />
+                  <Route path='/tile/:id/edit' component={TileForm} />
+                  <Route path='*' component={NotFound} />
+                </Switch>
+            }
+        </Fragment>
+      </Router>
     )
   }
 }
 
-export default App
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { handleInitialData }
+)(App)
