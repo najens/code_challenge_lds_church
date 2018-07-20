5e5ced77b91a133efd40802e3736924e34c014ef
Nate Jensen <najens@gmail.com>
2018-07-20 15:05:39 -0600

    docs: Create TileForm component

---
+++ src/components/TileForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class TileForm extends Component {
+	render () {
+		return (
+			<section>
+				Tile Form
+			</section>
+		)
+	}
+}
+
+export default TileForm
