3e2318c67010d71e641a2fd5a9c682ad266ad2ee
Nate Jensen <najens@gmail.com>
2018-07-20 14:53:11 -0600

    docs: Add unit tests for App component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,38 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../components/App'
+import { MemoryRouter } from 'react-router'
+import { App } from '../components/App'
+import Dashboard from '../components/Dashboard'
+import NotFound from '../components/NotFound'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+const mockFn = jest.fn()
+
+describe('rendering', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(<App handleInitialData={mockFn}/>)
+  })
+  it('should render without crashing', () => {
+    expect(wrapper).toHaveLength(1)
+  })
+  it('should render the snapshot', () => {
+    expect(shallowToJson(wrapper)).toMatchSnapshot()
+  })
+})
+
+describe('lifecycle', () => {
+  let wrapper
+	const spy = jest.spyOn(App.prototype, 'componentDidMount')
+  beforeEach(() => {
+    wrapper = shallow(<App handleInitialData={mockFn} />)
+  })
+  describe('when component is mounted', () => {
+    beforeEach(() => {
+      wrapper.instance().componentDidMount()
+    })
+		it('calls componentDidMount', () => {
+	    expect(spy).toHaveBeenCalled()
+	    spy.mockReset()
+	    spy.mockRestore()
+	  })
+  })
 })
