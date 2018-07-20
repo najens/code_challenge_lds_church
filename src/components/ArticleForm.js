5e55193078bed5031e212de34c71a98a58e5eb2c
Nate Jensen <najens@gmail.com>
2018-07-20 15:05:14 -0600

    docs: Create ArticleForm component

---
+++ src/components/ArticleForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class ArticleForm extends Component {
+	render () {
+		return (
+			<section>
+				Article Form
+			</section>
+		)
+	}
+}
+
+export default ArticleForm
