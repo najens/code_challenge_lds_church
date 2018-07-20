e1770208a79b6e14cebd94bc0c82699b691feb7c
Nate Jensen <najens@gmail.com>
2018-07-20 15:00:31 -0600

    docs: Create Login.js component

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Login extends Component {
+	render () {
+		return (
+			<section>
+				Log in
+			</section>
+		)
+	}
+}
+
+export default Login
